fcc49e34e18de44913b035460e8eb4c51a131cb0
Sean Roh <roh0sun@openobject.net>
2014-12-22 17:42:28 +0900

    VS2013 build output

---
+++ out/Debug/unreg.cmd
@@ -0,0 +1,10 @@
+regsvr32 -u -s vp8decoder.dll
+regsvr32 -u -s vp8encoder.dll
+rem regsvr32 -u -s vp9decoder.dll
+regsvr32 -u -s webmcc.dll
+regsvr32 -u -s webmmux.dll
+rem regsvr32 -u -s webmoggsource.dll
+regsvr32 -u -s webmsource.dll
+regsvr32 -u -s webmsplit.dll
+regsvr32 -u -s webmvorbisdecoder.dll
+regsvr32 -u -s webmvorbisencoder.dll
---
+++ out/Release/reg.cmd
@@ -0,0 +1,10 @@
+regsvr32 -s vp8decoder.dll
+regsvr32 -s vp8encoder.dll
+rem regsvr32 -s vp9decoder.dll
+regsvr32 -s webmcc.dll
+regsvr32 -s webmmux.dll
+rem regsvr32 -s webmoggsource.dll
+regsvr32 -s webmsource.dll
+regsvr32 -s webmsplit.dll
+regsvr32 -s webmvorbisdecoder.dll
+regsvr32 -s webmvorbisencoder.dll
